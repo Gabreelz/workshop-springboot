adf92e6d00c7e9331b54fd0f63079ff12cc4a56b
Gabreelz <gabriel_teixeira30@hotmail.com>
2025-08-31 18:12:26 -0300

    correction User Entity

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -8,8 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity 
-@Table(name = "tb/-user")
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
